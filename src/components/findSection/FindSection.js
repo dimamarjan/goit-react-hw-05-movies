054b7062fdfc7a289dcd143429001be5970a2465
dimamarjan <dima.marjan@gmail.com>
2021-08-02 13:49:36 +0300

    upd

--- src/components/findSection/FindSection.js
+++ src/components/findSection/FindSection.js
@@ -28,7 +28,6 @@ export function FindSection() {
 
   useEffect(() => {
     if (findingData) {
-      console.log(`useEffect`, 'ok');
       getFilmByName(findingData).then(resp => {
         setFilmsList(resp.data.results);
       });
