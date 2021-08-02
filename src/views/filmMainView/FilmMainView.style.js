48997398b096898fb01560828bb2690b4636b9ef
dimamarjan <dima.marjan@gmail.com>
2021-08-02 14:06:29 +0300

    upd

--- src/views/filmMainView/FilmMainView.style.js
+++ src/views/filmMainView/FilmMainView.style.js
@@ -1,1 +1,23 @@
 import styled from '@emotion/styled';
+
+export const MainFilmLogo = styled.img``;
+
+export const MainFilmHeader = styled.h1``;
+
+export const UsagePercentScore = styled.p``;
+
+export const OwerViewText = styled.p``;
+
+export const DescriptionText = styled.p``;
+
+export const GenresHeader = styled.p``;
+
+export const GenresText = styled.p``;
+
+export const AdditionalSection = styled.div``;
+
+export const AdditionalSectionHeader = styled.p``;
+
+export const AdditionalSectionList = styled.ul``;
+
+export const AdditionalSectionListItem = styled.li``;
